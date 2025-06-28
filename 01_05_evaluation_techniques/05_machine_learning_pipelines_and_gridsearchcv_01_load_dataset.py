52409292952046aea71f91c35b010c5a60b28f31
Pyeong-an <Pyeong-an@users.noreply.github.com>
2025-06-28 22:18:24 +0900

    Coursera - Data leakage and Pipeline

---
+++ 01_05_evaluation_techniques/05_machine_learning_pipelines_and_gridsearchcv_01_load_dataset.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+data = load_iris()
+X, y = data.data, data.target
+labels = data.target_names
